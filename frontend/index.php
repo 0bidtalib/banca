bd754c863fb30a443c5ad8065c76fd255c14ac20
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 20:48:50 +0200

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -1,45 +1,27 @@
 <?php
-    require_once '../frontend/header.php';
-    require_once '../backend/conn.php';
-
     if (!$_SESSION['logged']) {
         header("location: http://localhost/Progetto/frontend/login.php");
     }
-
+    require_once '../frontend/header.php';
+    require_once '../backend/conn.php';
     if ($_SESSION['user']=='admin' && $_SESSION['userID']==5 ) {
         $sqlEnt = "SELECT * FROM entrate WHERE inizio_periodo>='2023/06/01' AND fine_periodo<='2023/06/30'";
         $sqlUsc = "SELECT * FROM uscite WHERE data>='".date('Y-m-01')."' AND data<='".date('Y-m-t')."'";
-
-        $resEnt = $conn->query($sqlEnt);
-        $resUsc = $conn->query($sqlUsc);
-
-        $totEntrate = 0;
-        $totUscite = 0;
-
-        while($row = $resEnt->fetch_assoc()) {
-            $totEntrate += $row["stipendio"];
-        }
-
-        while($row = $resUsc->fetch_assoc()) {
-            $totUscite += $row["importo"];
-        }
     } else {
         $sqlEnt = "SELECT * FROM entrate WHERE utente=".$_SESSION['userID']." AND inizio_periodo>='2023/06/01' AND fine_periodo<='2023/06/30'";
         $sqlUsc = "SELECT * FROM uscite WHERE utente=".$_SESSION['userID']." AND data>='".date('Y-m-01')."' AND data<='".date('Y-m-t')."'";
-        
-        $resEnt = $conn->query($sqlEnt);
-        $resUsc = $conn->query($sqlUsc);
+    }
 
-        $totEntrate = 0;
-        $totUscite = 0;
-
-        while($row = $resEnt->fetch_assoc()) {
-            $totEntrate += $row["stipendio"];
-        }
-
-        while($row = $resUsc->fetch_assoc()) {
-            $totUscite += $row["importo"];
-        }
+    $resEnt = $conn->query($sqlEnt);
+    $totEntrate = 0;
+    while ($row = $resEnt->fetch_assoc()) {
+        $totEntrate += $row["stipendio"];
+    }
+    
+    $resUsc = $conn->query($sqlUsc);
+    $totUscite = 0;
+    while ($row = $resUsc->fetch_assoc()) {
+        $totUscite += $row["importo"];
     }
 ?>
     <h1>Home page</h1>
