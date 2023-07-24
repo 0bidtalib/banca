52b2a3dc237394e094bcf20492ae0d76ca1aa2c8
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:12:46 +0200

    Update register2.php

--- frontend/register2.php
+++ frontend/register2.php
@@ -1,12 +1,9 @@
-<?php
-    require_once '../frontend/header.php';
-?>
+<?php require_once '../frontend/header.php'; ?>
 <h1>Second stage</h1>
 <br><br>
 <p>Choose your username and password in order to utilize our services.</p>
 <br><br>
     <form action="http://localhost/Progetto/backend/register2.php" method="post">
-
         <label for="username">Username:</label>
         <input type="text" name="username" placeholder="email u selected earlier or a new username"><br><br>
 
@@ -15,6 +12,4 @@
 
         <input type="submit" value="Save">
     </form>
-<?php
-    require_once '../frontend/footer.php';
-?>
+<?php require_once '../frontend/footer.php'; ?>
