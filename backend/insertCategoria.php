046e3914897b96d6cf88d78e0703e2d64c50410d
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:15:36 +0200

    Update insertCategoria.php

--- backend/insertCategoria.php
+++ backend/insertCategoria.php
@@ -1,7 +1,6 @@
 <?php
     require_once '../backend/conn.php';
     $sql = 'INSERT INTO categorie(descrizione, key_word) VALUES ("'.$_GET['descrizione'].'", key_word="'.$_GET['key_word'].'")';
-
     if ($conn->query($sql) === true) {
         header("location: http://localhost/Progetto/frontend/categorie.php");
     } else {
