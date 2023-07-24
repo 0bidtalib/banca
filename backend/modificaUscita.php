f91796a37e0761d2a729cd0e1ea57d457a012f6e
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:22:05 +0200

    Update modificaUscita.php

--- backend/modificaUscita.php
+++ backend/modificaUscita.php
@@ -1,5 +1,10 @@
 <?php
-    require_once 'conn.php';
-    echo 'Modifica<br>';
-    echo 'id operazione: ' . $_GET['id'];
+    require_once '../backend/conn.php';
+    $sql = 'UPDATE uscite SET () VALUES WHERE id=';
+    if ($conn->query($sql) === true) {
+        header('header: http://localhost/Progetto/frontend/gestioneUscite.php';
+    } else {
+        echo 'Errore durante la modifica dell\'operazione: <br>';
+        echo $sql;
+    }
 ?>
