b4db2addcc79057d7844012653c430d07bc6eb12
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:16:24 +0200

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -1,9 +1,7 @@
 <?php
     require_once '../backend/conn.php';
     $sql = "SELECT * FROM loginfo WHERE (username='".$_POST['username']."' OR email='".$_POST['username']."') AND password='".$_POST['password']."'";
-
     $result = $conn->query($sql);
-    
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $_SESSION['logged'] = true;
