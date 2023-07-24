646e29452be3babea06428de4eed4a6c39fef579
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:15:48 +0200

    Update insertUscita.php

--- backend/insertUscita.php
+++ backend/insertUscita.php
@@ -1,7 +1,6 @@
 <?php
     require_once '../backend/conn.php';
     $sql = "INSERT INTO uscite (importo, descrizione, data, categoria, utente) VALUES (".$_POST['importo'].", '".$_POST['descrizione']."', '".$_POST['dataImporto']."', ".$_POST['categoria'].", ".$_SESSION['userID'].")";
-
     if ($conn->query($sql) === 'true') {
         header("location: http://localhost/Progetto/frontend/gestioneUscite.php");
     } else {
