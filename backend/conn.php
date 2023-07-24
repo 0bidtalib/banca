54efb5e0ac4254fc4130e570c344cc3d207738a8
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-24 21:14:03 +0200

    Update conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -1,9 +1,7 @@
 <?php
     session_start();
     require_once('db_dati.php');
-    $conn = new mysqli($server,
-    $username, $password, $nome_DB);
-
+    $conn = new mysqli($server, $username, $password, $nome_DB);
     if ($conn->connect_error) {
         echo "error: " + $conn->connect_error;
     }
