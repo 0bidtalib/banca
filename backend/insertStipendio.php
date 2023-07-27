5db14b44f81f4dfea62f6a2e5f421a8e557f8539
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-27 13:17:00 +0200

    Update insertStipendio.php

--- backend/insertStipendio.php
+++ backend/insertStipendio.php
@@ -2,12 +2,13 @@
     require_once '../backend/conn.php';
     $sql = "INSERT INTO entrate (stipendio, inizio_periodo, fine_periodo, utente) VALUES (".$_POST['stipendio'].", '".$_POST['inizio_periodo']."', '".$_POST['fine_periodo']."', '".$_SESSION['userID']."')";
 
-    echo $sql;
+    $res = $conn->query($sql);
 
-    // if ($conn->query($sql) === 'true') {
-    //     header("location: http://localhost/Progetto/frontend/insertStipendio.php");
-    // } else {
-    //     echo "something went wrong while inserting...<br>";
-    //     echo $sql;
-    // }
+    if ( $res === 'true') {
+        header("location: http://localhost/Progetto/frontend/insertStipendio.php");
+    } else {
+        echo $res . '<br>';
+        echo "something went wrong while inserting...<br>";
+        echo $sql;
+    }
 ?>
