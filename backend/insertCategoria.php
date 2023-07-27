04bf773c28e0a8de458aca53aaae2321725f1d75
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-27 13:14:11 +0200

    Update insertCategoria.php
    aggiunto un filtro che permette soltanto l'ADMIN ad aggiungere nuove categorie.

--- backend/insertCategoria.php
+++ backend/insertCategoria.php
@@ -1,10 +1,14 @@
 <?php
     require_once '../backend/conn.php';
-    $sql = 'INSERT INTO categorie(descrizione, key_word) VALUES ("'.$_GET['descrizione'].'", key_word="'.$_GET['key_word'].'")';
-    if ($conn->query($sql) === true) {
-        header("location: http://localhost/Progetto/frontend/categorie.php");
+    if ($_SESSION['userID'] == 5 && $_SESSION['username'] == 'admin')  {
+        $sql = 'INSERT INTO categorie(descrizione, key_word) VALUES ("'.$_GET['descrizione'].'", key_word="'.$_GET['key_word'].'")';
+        if ($conn->query($sql) === true) {
+            header("location: http://localhost/Progetto/frontend/categorie.php");
+        } else {
+            echo 'Errore nell\'inserimento della categoria: <br>';
+            echo $sql;
+        }
     } else {
-        echo 'Errore nell\'inserimento della categoria: <br>';
-        echo $sql;
+        echo 'You are not allowed to create a new category';
     }
 ?>
