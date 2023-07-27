3cfecbeb7b3f1509f6d417036dc66a3c0172a251
0bidtalib <118524210+0bidtalib@users.noreply.github.com>
2023-07-27 20:07:22 +0200

    added missing a paranthesis

--- backend/modificaUscita.php
+++ backend/modificaUscita.php
@@ -2,7 +2,7 @@
     require_once '../backend/conn.php';
     $sql = 'UPDATE uscite SET () VALUES WHERE id=';
     if ($conn->query($sql) === true) {
-        header('header: http://localhost/Progetto/frontend/gestioneUscite.php';
+        header('header: http://localhost/Progetto/frontend/gestioneUscite.php');
     } else {
         echo 'Errore durante la modifica dell\'operazione: <br>';
         echo $sql;
